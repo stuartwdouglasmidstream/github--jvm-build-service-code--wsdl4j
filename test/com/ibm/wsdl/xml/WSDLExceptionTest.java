f9354d6ccd2cb3d8febf8b67c9f9d17404384c21
t_e_v_a_n_s <>
2006-08-02 14:02:24 +0000

    Bug 1533106 - Don't catch Throwable, catch only checked exceptions

--- test/com/ibm/wsdl/xml/WSDLExceptionTest.java
+++ test/com/ibm/wsdl/xml/WSDLExceptionTest.java
@@ -7,7 +7,10 @@ package com.ibm.wsdl.xml;
 import javax.wsdl.Definition;
 import javax.wsdl.WSDLException;
 import javax.wsdl.factory.WSDLFactory;
+import javax.wsdl.xml.WSDLLocator;
 import javax.wsdl.xml.WSDLReader;
+
+import org.xml.sax.InputSource;
 
 import com.ibm.wsdl.factory.WSDLFactoryImpl3;
 
@@ -16,6 +19,33 @@ import junit.framework.TestCase;
 
 public class WSDLExceptionTest extends TestCase
 {
+  class MyWSDLLocator implements WSDLLocator
+  {  
+    public MyWSDLLocator()
+    {      
+    }
+
+    public InputSource getBaseInputSource()
+    {
+      throw new RuntimeException("Test Runtime Exception");
+    }
+
+    public InputSource getImportInputSource(String parentLocation, String importLocation)
+    {
+      throw new RuntimeException("Test Runtime Exception");
+    }
+
+    public String getBaseURI()
+    {
+      throw new RuntimeException("Test Runtime Exception");
+    }
+
+    public String getLatestImportURI()
+    {
+      throw new RuntimeException("Test Runtime Exception");
+    }
+  }
+
   public WSDLExceptionTest()
   {
     super("WSDLException");
@@ -56,5 +86,34 @@ public class WSDLExceptionTest extends TestCase
     toString = wsdlException.toString();
     
     assertEquals("String is not correct","javax.wsdl.WSDLException: WSDLException: faultCode=CONFIGURATION_ERROR: Test WSDL Exception: java.lang.IllegalArgumentException: Test IllegalArgument",toString);       
+  }
+
+  /**
+   * Test that RuntimeExceptions are thrown all the way up.
+   * 
+   * @throws Exception
+   */
+  public void testRuntimeException() throws Exception
+  {
+    WSDLFactory factory = WSDLFactory.newInstance();
+    WSDLReader reader = factory.newWSDLReader();
+  
+    reader.setFeature("javax.wsdl.verbose", true);
+    reader.setFeature("javax.wsdl.importDocuments", true);
+    
+    WSDLLocator myWSDLLocator = new MyWSDLLocator();
+    try
+    {
+      Definition def = reader.readWSDL(myWSDLLocator);
+    }
+    catch(WSDLException e)
+    {
+      fail("Should not have caught a WSDLException");
+    }    
+    catch(RuntimeException e)
+    {
+      //System.out.println("RuntimeException correctly thrown");
+      //this is what we wanted
+    }
   }  
 }
